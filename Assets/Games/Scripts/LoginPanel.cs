17cb44b31972b167f5d14e489c0ec4789263d312
arijeet baruah <arijeetbaruah@gmail.com>
2022-08-18 09:24:41 +0530

    test commond

--- Assets/Games/Scripts/LoginPanel.cs
+++ Assets/Games/Scripts/LoginPanel.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -18,10 +19,113 @@ public class LoginPanel : MonoBehaviour
         PlayFabAuthSystem.LoginWithCustomID(false, result =>
         {
             Debug.Log($"Login User: {result.PlayFabId}");
+
+            PlayFab.PlayFabClientAPI.ExecuteCloudScript(new PlayFab.ClientModels.ExecuteCloudScriptRequest
+            {
+                GeneratePlayStreamEvent = true,
+                FunctionName = "AcceptCommonGroupUser",
+                FunctionParameter = new Dictionary<string, object>
+                {
+                    { "GroupId", "5448014428B53D6F"},
+                    { "currentPlayerId", "556CF7422082128A"},
+                }
+            }, result =>
+            {
+                Debug.Log(result.ToJson());
+            }, ErrorCallback);
+
+
+            //PlayFab.PlayFabGroupsAPI.AcceptGroupApplication(new PlayFab.GroupsModels.AcceptGroupApplicationRequest
+            //{
+            //    Entity = new PlayFab.GroupsModels.EntityKey
+            //    {
+            //        Id = "17BD503CA4CDB5FE",
+            //        Type = "title_player_account"
+            //    },
+            //    Group = new PlayFab.GroupsModels.EntityKey
+            //    {
+            //        Id = "5448014428B53D6F",
+            //        Type = "group"
+            //    }
+            //}, result =>
+            //{
+            //    Debug.Log(result.ToJson());
+            //}, ErrorCallback);
+
+            //PlayFab.PlayFabGroupsAPI.ApplyToGroup(new PlayFab.GroupsModels.ApplyToGroupRequest
+            //{
+            //    Entity = new PlayFab.GroupsModels.EntityKey
+            //    {
+            //        Id = "17BD503CA4CDB5FE",
+            //        Type = "title_player_account"
+            //    },
+            //    Group = new PlayFab.GroupsModels.EntityKey
+            //    {
+            //        Id = "5448014428B53D6F",
+            //        Type = "group"
+            //    }
+            //}, result =>
+            //{
+            //    Debug.Log(result.ToJson());
+            //}, ErrorCallback);
+
         }, error =>
         {
             OpenRegister(false);
         });
+    }
+
+    public void CreateGroup()
+    {
+        PlayFab.PlayFabGroupsAPI.CreateGroup(new PlayFab.GroupsModels.CreateGroupRequest
+        {
+            GroupName = "Test Group"
+        }, result1 =>
+        {
+            Debug.Log($"Group Created: {result1.GroupName}");
+            PlayFab.PlayFabServerAPI.GetUserAccountInfo(new PlayFab.ServerModels.GetUserAccountInfoRequest
+            {
+                PlayFabId = "556CF7422082128A",
+            }, r =>
+            {
+                PlayFab.GroupsModels.EntityKey entityKey = new PlayFab.GroupsModels.EntityKey
+                {
+                    Id = r.UserInfo.TitleInfo.TitlePlayerAccount.Id,
+                    Type = r.UserInfo.TitleInfo.TitlePlayerAccount.Type
+                };
+
+                PlayFab.PlayFabGroupsAPI.InviteToGroup(new PlayFab.GroupsModels.InviteToGroupRequest
+                {
+                    Group = result1.Group,
+                    Entity = entityKey
+                }, result =>
+                {
+                    Debug.Log(result.ToJson());
+                    PlayFab.PlayFabGroupsAPI.AcceptGroupApplication(new PlayFab.GroupsModels.AcceptGroupApplicationRequest
+                    {
+                        Entity = entityKey,
+                        Group = result1.Group
+                    }, r2 =>
+                    {
+                        Debug.Log(r2.ToJson());
+                    }, ErrorCallback);
+
+                }, ErrorCallback);
+            }, null);
+            //PlayFab.PlayFabClientAPI.ExecuteCloudScript(new PlayFab.ClientModels.ExecuteCloudScriptRequest
+            //{
+            //    FunctionName = "AddCommonMembersInTitleGroup",
+            //    GeneratePlayStreamEvent = true,
+            //    FunctionParameter = new Dictionary<string, object>
+            //    {
+            //        { "GroupId", result1.Group.Id },
+            //        { "currentPlayerId", "556CF7422082128A" },
+            //    }
+            //}, result =>
+            //{
+            //    Debug.Log(result.ToJson());
+            //}, ErrorCallback);
+        }, ErrorCallback);
     }
 
     public void LoginWithEmail()
